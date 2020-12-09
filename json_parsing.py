8cb2302ba55bb730be7b60525e18e4225d339070
Andrew Miotke <andrewmiotke@gmail.com>
2020-12-08 16:20:53 -0800

    JSON: Wrote a small json parsing script

---
+++ json_parsing.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+"""
+A quick and simple script to read some data from
+a web API and parse json.
+"""
+
+def main():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    posts = json.loads(response.text)
+
+    for i in posts:
+        user = "userId"
+        title = i["title"]
+        body = i["body"]
+
+        if i[user] == 2:
+            print(f"Title:{title}\nBody:{body}\n")
+
+
+
+if __name__ == '__main__':
+    main()
